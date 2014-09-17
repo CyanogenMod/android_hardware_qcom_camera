a791cd32bf2471af328b528bbf79b2568387d9f7
Tom Marshall <tdm@cyngn.com>
2014-09-17 14:07:05 -0700

    camera: Use project pathmap
    Change-Id: I7c79b4117ebcea147c57afa19d81d632d07478db

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-camera))
+
 ifneq ($(strip $(USE_DEVICE_SPECIFIC_CAMERA)),true)
 ifneq ($(filter msm8960 msm8226 msm8974,$(TARGET_BOARD_PLATFORM)),)
   ifneq ($(USE_CAMERA_STUB),true)
@@ -6,4 +8,6 @@ ifneq ($(filter msm8960 msm8226 msm8974,$(TARGET_BOARD_PLATFORM)),)
     endif
   endif
 endif
+endif
+
 endif
